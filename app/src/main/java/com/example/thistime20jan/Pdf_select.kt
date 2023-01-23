02666a9d2d4db1f74c48a5e8b8ce29018628b9a5
OnurIlgaz <117534459+OnurIlgaz@users.noreply.github.com>
2023-01-23 03:45:31 +0300

    Bad things

--- app/src/main/java/com/example/thistime20jan/Pdf_select.kt
+++ app/src/main/java/com/example/thistime20jan/Pdf_select.kt
@@ -1,21 +1,19 @@
 package com.example.thistime20jan
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Pdfselect.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Pdfselect : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -28,32 +26,44 @@ class Pdfselect : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pdf_select, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Pdf_select.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Pdfselect().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    var requestCode : Int = 0
+    override fun onViewCreated(view: View, savedInstanceState:Bundle?) {
+        var buton : ImageView = requireView().findViewById(R.id.selection2)
+        buton.setOnClickListener {
+            val intent = Intent()
+                .setType("*/*")
+                .setAction(Intent.ACTION_GET_CONTENT)
+
+
+        }
     }
+    /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 111 && resultCode == RESULT_OK) {
+            val selectedFile = data?.data // The URI with the location of the file
+        }
+    }*/
+    fun openSomeActivityForResult() {
+        val intent = Intent(this, SomeActivity::class.java)
+        resultLauncher.launch(intent)
+        return intent
+    }
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // There are no request codes
+            val data: Intent? = result.data
+
+        }
+    }
+
 }
