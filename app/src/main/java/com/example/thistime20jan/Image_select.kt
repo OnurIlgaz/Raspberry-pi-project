5dd6adeb2a55f72a0e3af4712f0ef55d77e8310f
OnurIlgaz <117534459+OnurIlgaz@users.noreply.github.com>
2023-01-23 06:42:19 +0300

    Able to select files

--- app/src/main/java/com/example/thistime20jan/Image_select.kt
+++ app/src/main/java/com/example/thistime20jan/Image_select.kt
@@ -5,17 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-
 class Image_select : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var observer : MyLifecycleObserver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +24,8 @@ class Image_select : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        observer = MyLifecycleObserver(requireActivity().activityResultRegistry)
+        lifecycle.addObserver(observer)
     }
 
     override fun onCreateView(
@@ -33,4 +36,12 @@ class Image_select : Fragment() {
         return inflater.inflate(R.layout.fragment_image_select, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val selectButton = view.findViewById<Button>(R.id.selection2)
+
+        selectButton.setOnClickListener {
+            // Open the activity to select an image
+            observer.selectImage()
+        }
+    }
 }
--- app/src/main/java/com/example/thistime20jan/Pdf_select.kt
+++ app/src/main/java/com/example/thistime20jan/Pdf_select.kt
@@ -5,21 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Pdfselect.newInstance] factory method to
- * create an instance of this fragment.
- */
+class MyLifecycleObserver(private val registry : ActivityResultRegistry)
+    : DefaultLifecycleObserver {
+    lateinit var getContent : ActivityResultLauncher<String>
+
+    override fun onCreate(owner: LifecycleOwner) {
+        getContent = registry.register("key", owner, ActivityResultContracts.GetContent()) { uri ->
+            // Handle the returned Uri
+        }
+    }
+
+    fun selectPdf() {
+        getContent.launch("pdf/*")
+    }
+    fun selectImage() {
+        getContent.launch("image/*")
+    }
+}
+
 class Pdfselect : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var observer : MyLifecycleObserver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +47,8 @@ class Pdfselect : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        observer = MyLifecycleObserver(requireActivity().activityResultRegistry)
+        lifecycle.addObserver(observer)
     }
 
     override fun onCreateView(
@@ -37,23 +59,12 @@ class Pdfselect : Fragment() {
         return inflater.inflate(R.layout.fragment_pdf_select, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Pdf_select.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Pdfselect().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val selectButton = view.findViewById<Button>(R.id.selection1)
+
+        selectButton.setOnClickListener {
+            // Open the activity to select an image
+            observer.selectPdf()
+        }
     }
 }
