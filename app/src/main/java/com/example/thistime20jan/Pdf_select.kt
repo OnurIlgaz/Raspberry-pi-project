49bf6d89b5d6071097cd1b1ba3b911df36309973
OnurIlgaz <117534459+OnurIlgaz@users.noreply.github.com>
2023-01-23 04:29:14 +0300

    Added button and deleted trash

--- app/src/main/java/com/example/thistime20jan/Pdf_select.kt
+++ app/src/main/java/com/example/thistime20jan/Pdf_select.kt
@@ -1,10 +1,12 @@
 package com.example.thistime20jan
 
+import android.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -37,23 +39,4 @@ class Pdfselect : Fragment() {
         return inflater.inflate(R.layout.fragment_pdf_select, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Pdf_select.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Pdfselect().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
